0902813f67f10f24bb5c94250619226c16dda962
James Baker <bakerjm702@gmail.com>
2022-10-01 18:11:27 -0700

    Initial commit

---
+++ src/components/Routes.tsx
@@ -0,0 +1,9 @@
+import { Route } from 'wouter';
+
+import Home from './Resume';
+
+const Routes = () => (
+	<Route path="/" component={Home} />
+);
+
+export default Routes;
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import preact from '@preact/preset-vite'
+import eslint from 'vite-plugin-eslint'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  	plugins: [preact(), eslint()],
+  	server: {
+		open: true,
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+		},
+	},
+})
