7b8f919d5c9add6e326880e2b8e77d4d07603362
James Baker <bakerjm702@gmail.com>
2022-10-20 14:22:32 -0700

    Fixed serving in Windows. Fixed up print styling.

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig({
     resolve: {
         alias: [
             { 
-                find: '@', 
-                replacement: path.resolve(__dirname, '/src'),
+                find: '@',
+                replacement: path.resolve(__dirname, './src'),
             }
         ],
     },
